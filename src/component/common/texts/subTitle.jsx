f7c85f0290ac702a6dd1fc2a9c652fdc8a2ea025
imstandup94 <imstandup94@gmail.com>
2022-06-24 22:03:09 +0900

    Updates

--- src/component/common/texts/subTitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './subTitle.module.css';
-
-const SubTitle = React.memo(({subTitle, style}) => (
-    <p className={styles.subTitle} style={style}>{subTitle}</p>
-));
-
-export default SubTitle;
--- src/component/page/viewProject/common/designTitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './designTitle.module.css';
-
-const DesignTitle = ({text}) => (
-    <h6 className={styles.text}>{text}</h6>
-);
-
-export default DesignTitle;
